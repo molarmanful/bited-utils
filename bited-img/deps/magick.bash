bf0c5df20da0d7daaf9ca9761cef3342d491ae3e
molarmanful <ben@benpa.ng>
2025-01-24 15:03:30 -0500

    no more extraneous precalc

--- bited-img/deps/magick.bash
+++ bited-img/deps/magick.bash
@@ -6,11 +6,8 @@ fsz="$5"
 bg="$6"
 fg="$7"
 
-magick -font "$ttf" -pointsize "$fsz" label:@"$txt" "$out".png
-
 magick \
   -background "$bg" -fill "$fg" +antialias \
-  -size "$(identify -ping -format '%wx%h' "$out".png)" \
   pango:@"$pango" \
   -bordercolor "$bg" -border "$fsz" \
   "$out".png
