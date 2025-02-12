4e172a12191d5ba2d71647e1ff71294868d17a9a
molarmanful <ben@benpa.ng>
2025-02-11 20:17:52 -0500

    psfu.gz -> consolefonts

--- template/default.nix
+++ template/default.nix
@@ -24,11 +24,12 @@ stdenvNoCC.mkDerivation {
 
   installPhase = ''
     runHook preInstall
-    mkdir -p $out/share/fonts
+    mkdir -p $out/share/fonts $out/share/consolefonts
     cp -r out/. $out/share/fonts
     ${lib.optionalString release ''
       ${zip}/bin/zip -r $out/share/fonts/${pname}_${version}.zip $out/share/fonts
     ''}
+    mv out/*.psfu.gz $out/share/consolefonts
     runHook postInstall
   '';
 }
