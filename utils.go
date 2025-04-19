785d867043006517a0445bf5640f9924bd123d66
molarmanful <ben@benpa.ng>
2025-04-19 00:38:09 -0400

    doc: separate doc for base

--- utils.go
+++ utils.go
@@ -1,13 +1,3 @@
-// bited-utils is a set of pipeline helpers and utilities for building fonts
-// from bited BDFs.
-//
-// Documentation for each utility:
-//
-//   - [github.com/molarmanful/bited-utils/bited-build]
-//   - [github.com/molarmanful/bited-utils/bited-img]
-//   - [github.com/molarmanful/bited-utils/bited-clr]
-//   - [github.com/molarmanful/bited-utils/bited-scale]
-//   - [github.com/molarmanful/bited-utils/bited-bbl]
 package bitedutils
 
 import (
