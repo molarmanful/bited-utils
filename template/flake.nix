27dafe9105cdb3e908953a30421f759403a0eaff
molarmanful <ben@benpa.ng>
2025-04-19 23:22:20 -0400

    refactor(template): clean up flake

--- template/flake.nix
+++ template/flake.nix
@@ -2,23 +2,33 @@
   description = "A cool font waiting to be built";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    systems.url = "systems";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    devshell.url = "github:numtide/devshell";
-    bited-utils.url = "github:molarmanful/bited-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    bited-utils = {
+      url = "github:molarmanful/bited-utils";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+      };
+    };
   };
 
   outputs =
     inputs@{ systems, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        inputs.devshell.flakeModule
-        inputs.bited-utils.flakeModule
-      ];
+      imports = [ inputs.bited-utils.flakeModule ];
       systems = import systems;
       perSystem =
-        { config, pkgs, ... }:
+        {
+          config,
+          pkgs,
+          self',
+          ...
+        }:
         {
 
           bited-utils = {
@@ -31,45 +41,33 @@
 
           # Devtools available via `nix develop` or direnv.
           # Add or remove as you wish.
-          devshells.default = {
-
-            commands = with pkgs; [
-              {
-                package = nil;
-                category = "lsp";
-              }
-              {
-                package = nixd;
-                category = "lsp";
-              }
-              {
-                package = nixfmt-rfc-style;
-                category = "formatter";
-              }
-              {
-                package = statix;
-                category = "linter";
-              }
-              {
-                package = deadnix;
-                category = "linter";
-              }
-              { package = taplo; }
-              {
-                package = marksman;
-                category = "lsp";
-              }
-              {
-                package = mdformat;
-                category = "formatter";
-              }
-              { package = config.bited-utils.bited-clr; }
-            ];
-
+          devShells.default = pkgs.mkShell {
             packages = with pkgs; [
-              python313Packages.mdformat-gfm
-              python313Packages.mdformat-gfm-alerts
+              config.bited-utils.bited-clr
+              taplo
+              # lsps
+              nil
+              nixd
+              marksman
+              # formatters
+              nixfmt-rfc-style
+              mdformat
+              python3Packages.mdformat-gfm
+              python3Packages.mdformat-gfm-alerts
+              # linters
+              statix
+              deadnix
             ];
+          };
+
+          formatter = pkgs.writeShellApplication {
+            name = "linter";
+            runtimeInputs = self'.devShells.default.nativeBuildInputs;
+            text = ''
+              find . -iname '*.nix' -exec nixfmt {} \; -exec deadnix -e {} \; -exec statix fix {} \;
+              find . -iname '*.toml' -exec taplo fmt {} \;
+              find . -iname '*.md' -exec mdformat {} \;
+            '';
           };
         };
     };
